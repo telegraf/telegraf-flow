2ba0d9a15171c70bdaad28ac67309c2fafc5bee0
Vitaly Domnikov <dotcypress@gmail.com>
2016-07-29 19:10:13 -0700

    refactoring & quiz-flow

--- examples/simple-bot.js
+++ examples/simple-bot.js
@@ -1,6 +1,50 @@
 const Telegraf = require('telegraf')
 const TelegrafFlow = require('../lib/telegraf-flow')
-const Flow = TelegrafFlow.Flow
+const { Flow, QuizFlow } = TelegrafFlow
+
+// More info: https://github.com/telegraf/kwiz
+const beverageQuizDefinition = {
+  questions: [
+    {
+      message: 'What is your name?',
+      answer: { type: 'string', id: 'name' }
+    },
+    {
+      message: 'Got it!\n{{answers.name}}, how old are you?',
+      answer: { type: 'int', id: 'age' }
+    },
+    {
+      message: 'Coke or Pepsi?',
+      answer: { type: 'choise', items: ['Coke', 'Pepsi'], id: 'beverage' },
+      criteria: { 'answers.age': {$lt: 21} },
+      attachment: {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: 'Coke', callback_data: 'Coke' },
+              { text: 'Pepsi', callback_data: 'Pepsi' }
+            ]
+          ]
+        }
+      }
+    },
+    {
+      message: 'Beer or Vine?',
+      answer: { type: 'choise', items: ['Beer', 'Vine'], id: 'beverage' },
+      criteria: { 'answers.age': {$gte: 21} },
+      attachment: {
+        reply_markup: {
+          keyboard: [['Beer', 'Vine']],
+          one_time_keyboard: true,
+          resize_keyboard: true
+        }
+      }
+    },
+    {
+      message: 'Buy {{answers.name}}'
+    }
+  ]
+}
 
 const telegraf = new Telegraf(process.env.BOT_TOKEN)
 const telegrafFlow = new TelegrafFlow()
@@ -11,29 +55,25 @@ telegraf.use(Telegraf.memorySession())
 // Register middleware
 telegraf.use(telegrafFlow.middleware())
 
-const defaultFlow = new Flow()
-
-defaultFlow.on('message', (ctx) => {
-  return ctx.flow.start('deadbeef')
-})
-
 // Set default flow
+const defaultFlow = new Flow()
+defaultFlow.command('/start', (ctx) => ctx.flow.start('deadbeef'))
+defaultFlow.command('/quiz', (ctx) => ctx.flow.start('beverage'))
+defaultFlow.onResult((ctx) => ctx.reply(JSON.stringify(ctx.flow.result.answers, null, 2)))
 telegrafFlow.setDefault(defaultFlow)
 
 // Example flow
 const dummyFlow = new Flow('deadbeef')
-
-dummyFlow.onStart((ctx) => ctx.reply(ctx.state.flow.message || 'Hi'))
-
+dummyFlow.onStart((ctx) => ctx.reply(ctx.flow.state.message || 'Hi'))
+dummyFlow.onResult((ctx) => ctx.reply(JSON.stringify(ctx.flow.result.answers, null, 2)))
 dummyFlow.on('text', (ctx) => {
   if (ctx.message.text.toLowerCase() === 'hi') {
-    ctx.flow.stop()
-    return ctx.reply('Buy')
+    return ctx.flow.start('beverage')
   }
-  return ctx.flow.restart({message: 'Really?'})
+  return ctx.flow.restart({message: 'Hello'})
 })
 
-// Register flow
 telegrafFlow.register(dummyFlow)
+telegrafFlow.register(new QuizFlow('beverage', beverageQuizDefinition))
 
 telegraf.startPolling(30)
