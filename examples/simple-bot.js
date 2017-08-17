9435b0568e5ec0f5c1040d453657c1c02df87c35
Vitaly Domnikov <dotcypress@gmail.com>
2017-08-16 23:12:12 -0700

    update examples

--- examples/simple-bot.js
+++ examples/simple-bot.js
@@ -4,11 +4,6 @@ const { Scene, enter, leave } = TelegrafFlow
 
 const flow = new TelegrafFlow()
 
-// Global commands
-flow.command('help', (ctx) => ctx.reply('Help message'))
-flow.command('greeter', enter('greeter'))
-flow.command('echo', enter('echo'))
-
 // Greeter scene
 const greeterScene = new Scene('greeter')
 greeterScene.enter((ctx) => ctx.reply('Hi'))
@@ -19,6 +14,7 @@ greeterScene.on('message', (ctx) => ctx.reply('Send `hi`'))
 // Echo scene
 const echoScene = new Scene('echo')
 echoScene.enter((ctx) => ctx.reply('echo scene'))
+echoScene.leave((ctx) => ctx.reply('exiting echo scene'))
 echoScene.command('back', leave())
 echoScene.on('text', (ctx) => ctx.reply(ctx.message.text))
 echoScene.on('message', (ctx) => ctx.reply('Only text messages please'))
@@ -29,4 +25,7 @@ flow.register(greeterScene, echoScene)
 const app = new Telegraf(process.env.BOT_TOKEN)
 app.use(Telegraf.memorySession())
 app.use(flow.middleware())
+app.command('help', (ctx) => ctx.reply('Help message'))
+app.command('greeter', enter('greeter'))
+app.command('echo', enter('echo'))
 app.startPolling()
